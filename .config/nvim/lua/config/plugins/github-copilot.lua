012fa18177221c539cd337444089e360d6c9edd6
Marcus Hynfield <marcus@hynfield.org>
2025-06-25 11:56:56 -0400

    Replace copilot.vim with copilot.lua

--- .config/nvim/lua/config/plugins/github-copilot.lua
+++ .config/nvim/lua/config/plugins/github-copilot.lua
@@ -1,3 +1,17 @@
 return {
-  "github/copilot.vim",
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = "<Tab>",
+          accept_word = "<S-Tab>",
+          accept_line = "<M-Tab>",
+        },
+      },
+    })
+  end,
 }
